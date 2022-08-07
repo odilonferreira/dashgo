714f767db9a01c5aafb43767bc5c066495f7a4c8
Odilon Ferreira <odilon.aferreira@gmail.com>
2022-08-07 12:57:02 -0300

    install react hook form and control auth form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
-import Input from "../components/Form/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Input } from "../components/Form/Input";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -12,13 +26,25 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            // name="email"
+            type="email"
+            label="E-mail"
+            {...register("email")}
+          />
+          <Input type="password" label="Senha" {...register("password")} />
         </Stack>
 
-        <Button type="submit" marginTop={6} colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          marginTop={6}
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
